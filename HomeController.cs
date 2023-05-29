f1c991e51a5f6b3b92ffc54d28e616b13ec12152
Christopher Peguero <67020723+MrJefe27@users.noreply.github.com>
2023-05-29 00:01:38 -0400

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROYECTO_ITLA.Models;
+using System.Diagnostics;
+
+namespace PROYECTO_ITLA.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        public IActionResult Pilares()
+        {
+            return View();
+        }
+
+        public IActionResult Aprendizaje()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
